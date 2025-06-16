660660d739585c3fba7226fb2caa5ee1ec033a60
NileshVirkar148 <75842704+NileshVirkar148@users.noreply.github.com>
2025-06-16 20:09:57 +0530

    Update HelloWorld.java

--- java-maven/src/main/java/com/example/HelloWorld.java
+++ java-maven/src/main/java/com/example/HelloWorld.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Maven!");
-        try {                                                      
+        try {                                                           
             ObjectMapper mapper = new ObjectMapper();
             System.out.println("Jackson version: " + mapper.getClass().getPackage().getImplementationVersion());
             System.out.println("text");
